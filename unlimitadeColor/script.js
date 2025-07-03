c7096e050a0a4eb73b7144d289c1ee226822662f
akhiljha231 <jhaakhilkumar37@gmail.com>
2025-07-03 17:43:53 +0530

    add unlimitadeColor project

---
+++ unlimitadeColor/script.js
@@ -0,0 +1,28 @@
+const randomColor = function(){
+    const hex ='0123456789ABCDEF';
+    let color ='#';
+    for (let i = 0; i < 16; i++) {
+        color += hex[Math.floor(Math.random()*16)];
+     }
+     return color;
+}
+let intervalid;
+
+const startchangingcolor = function(){
+    if (!intervalid) {
+        intervalid = setInterval(changeBgColor,1000);
+    }
+
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+
+const stopChangingColor = function(){
+    clearInterval(intervalid);
+    intervalid=null;
+};
+document.querySelector('#start').addEventListener('click',startchangingcolor);
+document.querySelector('#stop').addEventListener('click',stopChangingColor);
+
+
